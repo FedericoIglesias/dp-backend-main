40ef07cbb0e56008da5e51aeb24cb1927ba75168
FedericoIglesias <igl.fede@gmail.com>
2024-07-26 09:02:53 +0000

    addSecurityWithJwtInGateway

--- gateway/src/main/java/dhm/gateway/filter/Filter.java
+++ gateway/src/main/java/dhm/gateway/filter/Filter.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -15,7 +17,16 @@ public class Filter implements GlobalFilter{
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
-        exchange.getRequest().getHeaders().getc
+        if(!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
+            throw new RuntimeException("Missing authorization header");
+        }
+
+        String jwt = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0).substring(7);
+
+
+
+        System.out.println(jwt);
+
 
         return chain.filter(exchange);
     }
