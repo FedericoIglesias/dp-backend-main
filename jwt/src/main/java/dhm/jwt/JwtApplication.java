aa4609dddb94960708dbe2a598455c4c2579fde8
FedericoIglesias <igl.fede@gmail.com>
2024-08-14 08:47:02 +0000

    doneDocJWT

--- jwt/src/main/java/dhm/jwt/JwtApplication.java
+++ jwt/src/main/java/dhm/jwt/JwtApplication.java
@@ -1,7 +1,10 @@
 package dhm.jwt;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JwtApplication {
@@ -10,4 +13,13 @@ public class JwtApplication {
 		SpringApplication.run(JwtApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenApi() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("DHM Api JWT")
+						.version("0.1")
+						.description("Endpoints of api JWT")
+				);
+	}
 }
--- jwt/src/main/java/dhm/jwt/controller/JwtController.java
+++ jwt/src/main/java/dhm/jwt/controller/JwtController.java
@@ -2,6 +2,11 @@ package dhm.jwt.controller;
 
 import dhm.jwt.dto.Users;
 import dhm.jwt.service.JwtService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,21 +17,37 @@ public class JwtController {
     @Autowired
     JwtService serviJwt;
 
+    @Operation(summary = "JWT", description = "This endpoint jwt")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Deliveries a token"),
+    })
     @GetMapping("/token")
     public String getToken(@RequestParam String username,@RequestParam Integer id){
         return serviJwt.getToken(username, id);
     }
 
+    @Operation(summary = "JWT", description = "This endpoint jwt")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Deliveries the name into from token"),
+    })
     @GetMapping("/username")
     public String getUsernameFromToken(@RequestParam String token){
         return serviJwt.getUsernameFromToken(token);
     }
 
+    @Operation(summary = "JWT", description = "This endpoint jwt")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Deliveries the id into from token"),
+    })
     @GetMapping("/id")
     public String getIdFromToken(@RequestParam String token){
         return serviJwt.getIdFromToken(token);
     }
 
+    @Operation(summary = "JWT", description = "This endpoint jwt")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Check if the token wasn't expired"),
+    })
     @GetMapping("/validate")
     public Boolean isValidate(@RequestParam String token){
         return serviJwt.isTokenExpired(token);
