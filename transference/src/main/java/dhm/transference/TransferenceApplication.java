8f99e985518a0001bc1a401d7d61fe2d2ab50bc2
FedericoIglesias <igl.fede@gmail.com>
2024-08-16 23:17:54 +0200

    modifyTransference

--- transference/src/main/java/dhm/transference/TransferenceApplication.java
+++ transference/src/main/java/dhm/transference/TransferenceApplication.java
@@ -1,7 +1,10 @@
 package dhm.transference;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TransferenceApplication {
@@ -10,4 +13,13 @@ public class TransferenceApplication {
 		SpringApplication.run(TransferenceApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenApi() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("DHM Api Register")
+						.version("0.1")
+						.description("Endpoints of api register")
+				);
+	}
 }
--- transference/src/main/java/dhm/transference/controller/TransferenceController.java
+++ transference/src/main/java/dhm/transference/controller/TransferenceController.java
@@ -2,6 +2,11 @@ package dhm.transference.controller;
 
 import dhm.transference.model.Transference;
 import dhm.transference.service.TransferenceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +24,21 @@ public class TransferenceController {
         return new ResponseEntity<>("STATUS UP", HttpStatus.OK);
     }
 
+    @Operation(summary = "Trasnference", description = "This endpoint deliveries transference")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Get transference",
+            content = @Content(
+                schema = @Schema(implementation = Transference.class))),
+    })
     @GetMapping
     public Transference getTransaction(@RequestParam Integer id) {
         return serviTransaction.getTransaction(id);
     }
 
+    @Operation(summary = "Trasnference", description = "This endpoint create transference")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Save transference")
+    })
     @PostMapping
     public void saveTransaction(@RequestBody Transference transference) {
         serviTransaction.saveTransaction(transference);
