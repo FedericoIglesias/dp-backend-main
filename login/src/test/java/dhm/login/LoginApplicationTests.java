c373b5d577cd63f81d877161fb474d85c2c3798d
FedericoIglesias <igl.fede@gmail.com>
2024-08-20 15:23:39 +0200

    addTestLogin

--- login/src/test/java/dhm/login/LoginApplicationTests.java
+++ login/src/test/java/dhm/login/LoginApplicationTests.java
@@ -1,13 +1,32 @@
 package dhm.login;
 
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 
 @SpringBootTest
 class LoginApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void loginTest() {
+    RestAssured.baseURI = "http://localhost:8082/api/login";
 
+    given()
+        .when()
+        .get("/status")
+        .then()
+        .statusCode(200);
+
+    given()
+        .header("Content-Type", "application/json")
+        .body("{\"email\":\"fede@gmail.com\",\"password\":\"holis\"}")
+        //.body(requestBody)
+			  .when()
+        .post()
+        .then()
+        .statusCode(200);
+  }
 }
